cbfe33044bad80037c05b990d6100a08d92e1bee
alejandroolmed59 <00097017@uca.edu.sv>
2021-06-16 23:58:54 -0600

    disennio mas cools

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
+import "antd/dist/antd.css";
 import './App.css'
 import { Component } from 'react';
-import { Form, Input } from 'reactstrap';
+import { Form } from 'reactstrap';
 import FileSaver from 'file-saver';
 import {CifrarDegenere ,DescifrarDegenere } from './seguridad/Degenere';
 import {GenerateJWT, ValidateJWT} from './seguridad/Jwt'
+import { Alert, Button, Input, Tag } from 'antd';
+import { KeyOutlined, UnlockOutlined } from '@ant-design/icons';
+
 
 const initState = {
   key: "",
   ciphed: "",
   txt: "",
-  lectura: "123",
+  lectura: "",
   myfile: null,
   archivo:"",
-  jwt:{}
+  jwt:{},
+  aler:false
 }
 
 class App extends Component {
@@ -40,6 +44,8 @@ class App extends Component {
   }
 
   Txt2XML =  txt => {
+    console.log("Hace xml")
+
     var xml = '<cliente> \n'
     var i = 0;
     var categorias = ["<documento>", "</documento>", "<primer-nombre>", "</primer-nombre>", "<apellido>", "</apellido>", "<credit-card>", "</credit-card>", "<tipo>", "</tipo>", "<telefono>", "</telefono>"];
@@ -47,10 +53,12 @@ class App extends Component {
     txt.split(';').forEach((param, k) => {
       if(k==3){ 
         //TODO VIGENERE
-        const creditCardChiped = CifrarDegenere(param, this.state.key)
-        this.state.ciphed = creditCardChiped
-
-        xml += '   ' + categorias[i] + creditCardChiped + categorias[i + 1] + '\n'
+        if(this.state.key=="") xml += '   ' + categorias[i] + param.trim() + categorias[i + 1] + '\n'
+        else{
+          const creditCardChiped = CifrarDegenere(param, this.state.key)
+          this.state.ciphed = creditCardChiped
+          xml += '   ' + categorias[i] + creditCardChiped + categorias[i + 1] + '\n'
+        }
       }
       else{
         xml += '   ' + categorias[i] + param.trim() + categorias[i + 1] + '\n'
@@ -64,6 +72,7 @@ class App extends Component {
     })
   }
   Txt2JSON = async(txt) => {
+    console.log("Hace json")
     var json = {};
     var categorias = ["documento", "primer-nombre", "apellido", "credit-card", "tipo", "telefono"];
 
@@ -71,18 +80,21 @@ class App extends Component {
         json[categorias[i]] = param.trim();
     })
     this.setState({json: json, archivo: JSON.stringify(json)})
-    GenerateJWT(json, this.state.key).then(jwt=>{
-        localStorage.setItem('jwtSesion', jwt);
-       this.setState({jwt})
-     })
+
+    if(this.state.key!==""){
+      GenerateJWT(json, this.state.key).then(jwt=>{
+          localStorage.setItem('jwtSesion', jwt);
+          this.setState({jwt})
+      })
+    }else{this.state.alert=true}
      
     
 }
-  submitHandler = e => {
+  submitHandler = (e, target) => {
     e.preventDefault()
-    if(e.target.id==="descargarXml"){
+    if(target==="descargarXml"){
       var file = new File([this.state.xml], "pruebisss.xml", {type: "application/xml"});
-    }else if(e.target.id==="descargarJson"){
+    }else if(target==="descargarJson"){
       var file = new File([JSON.stringify(this.state.json)], "pruebisss.json", {type: "application/json"});
     }
     FileSaver.saveAs(file);
@@ -92,14 +104,10 @@ class App extends Component {
     const decifrado = DescifrarDegenere(this.state.ciphed, this.state.key)
     console.log("DECIFRADO: "+ decifrado)
     this.setState({
-      ciphed: decifrado
+      displayCifrado: decifrado
     })
   }
-  changeLabel = e  =>{
-    const archivo = e.target.id
-    if(archivo==="generateJson") this.Txt2JSON(this.state.lectura)
-    else if(archivo==="generateXml") this.Txt2XML(this.state.lectura)
-  }
+
   validarJwt = e=>{
     e.preventDefault()
     if(localStorage.getItem("jwtSesion")!==null){
@@ -111,40 +119,37 @@ class App extends Component {
   }
 
   render() {
+
     return (
+      
       <div className="App">
+        {this.state.alert==""&& <Alert
+                    message="Error"
+                    description="Para generar el JWT es necesario tener un secreto"
+                    type="error"
+                    showIcon
+                />}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
           <Form>
-            <Input size={50} className='transparentInput' type="text" name="key" id="key" onChange={this.changeHandler} placeholder="Ingresa" onChange={this.changeHandler} value={this.state.key} />
+            <Input prefix={<KeyOutlined/>}  name="key" id="key" placeholder="Llave ultra secreta 🤐" onChange={this.changeHandler} value={this.state.key} />
             <Input type="file" id="myfile" name="myfile" onChange={(e) => this.changeFile(e)}  /> <br />
             <div>
-              <button id="descargarXml" onClick={e=>this.submitHandler(e)} >Descargar xml hey 🥴 </button>
-              <button id="descargarJson" onClick={e=>this.submitHandler(e)} >Descargar json hey 🥴 </button> <br />
+              <Button type="primary" name="generateXml" id="generateXml" onClick={()=>this.Txt2XML(this.state.lectura)} >General xml 🥴 </Button>
+              <Button type="primary" name="generateJson" id="generateJson" onClick={()=> this.Txt2JSON(this.state.lectura)}>Generar json 🥴 </Button>
             </div>
-            <button onClick={this.descifrarHandler} >Descifre hey 🥴 </button>
-            <button onClick={this.validarJwt} >Validar JWT 🥴 </button>
-            <label type="text" name="ciphed" id="ciphed">{this.state.ciphed}</label>
-            
+            <div>
+              <Button type="primary" id="descargarXml" onClick={(e)=>this.submitHandler(e,"descargarXml")} >Descargar xml hey 🥴 </Button>
+              <Button type="primary" id="descargarJson" onClick={(e)=>this.submitHandler(e,"descargarJson")} >Descargar json hey 🥴 </Button> <br />
+            </div>
+            <Button type="primary" onClick={this.descifrarHandler} >Descifre hey 🥴 </Button>
+            <Button type="primary" onClick={this.validarJwt} >Validar JWT 🥴 </Button>
           </Form>
           <label type="text" name="archivo" id="archivo">{this.state.archivo}</label><br />
+          <Tag  color="volcano" icon={<UnlockOutlined />} type="text" name="ciphed" id="ciphed">{this.state.displayCifrado}</Tag><br />
+          
+
           <div>
-            <button id="generateJson" onClick={e => this.changeLabel(e)} >Generar json 🥴 </button>
-            <button id="generateXml" onClick={e=> this.changeLabel(e)} >General xml 🥴 </button>
-          </div>
-          <div>
-            <button id="jwtLocal" onClick={e => this.validarJwt(e)} >Generar json del jwt almacenado en navegador🥴 </button>
+            <Button type="primary" id="jwtLocal" onClick={e => this.validarJwt(e)} >Generar json del jwt almacenado en navegador🥴 </Button>
           </div>
         </header>
       </div>
